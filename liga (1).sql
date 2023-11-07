41f26b29f70063ea6c9027e9f3c56afa509ad3d1
Daniel Blanco <34427276+dablara@users.noreply.github.com>
2023-11-07 12:34:38 +0100

    Add files via upload

---
+++ liga (1).sql
@@ -0,0 +1,81 @@
+-- MariaDB dump 10.18  Distrib 10.5.7-MariaDB, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: liga2asir
+-- ------------------------------------------------------
+-- Server version	10.5.6-MariaDB-1:10.5.6+maria~buster
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+DROP DATABASE IF EXISTS liga2asir;
+CREATE DATABASE IF NOT EXISTS liga2asir DEFAULT CHARACTER SET utf8mb4;
+
+USE liga2asir;
+
+--
+-- Table structure for table `equipo`
+--
+
+DROP TABLE IF EXISTS `equipo`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `equipo` (
+  `ID_equipo` int(11) NOT NULL auto_increment,
+  `Nombre_equipo` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`ID_equipo`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `equipo`
+--
+
+LOCK TABLES `equipo` WRITE;
+/*!40000 ALTER TABLE `equipo` DISABLE KEYS */;
+/*!40000 ALTER TABLE `equipo` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `jugador`
+--
+
+DROP TABLE IF EXISTS `jugador`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `jugador` (
+  `ID_jugador` int(11) NOT NULL auto_increment,
+  `Nombre_equipo` varchar(50) DEFAULT NULL,
+  `ID_equipo` int(11) DEFAULT NULL,
+  PRIMARY KEY (`ID_jugador`),
+  KEY `jugador_fk` (`ID_equipo`),
+  CONSTRAINT `jugador_fk` FOREIGN KEY (`ID_equipo`) REFERENCES `equipo` (`ID_equipo`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `jugador`
+--
+
+LOCK TABLES `jugador` WRITE;
+/*!40000 ALTER TABLE `jugador` DISABLE KEYS */;
+/*!40000 ALTER TABLE `jugador` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-11-12  9:22:41
